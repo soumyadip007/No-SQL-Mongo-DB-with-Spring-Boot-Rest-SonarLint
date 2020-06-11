cca79879c8c03907d95ce7896fb15b0262788a9b
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-11 11:23:34 +0530

    Mongo App

--- mongo/src/main/java/com/spring/mongo/MongoApplication.java
+++ mongo/src/main/java/com/spring/mongo/MongoApplication.java
@@ -2,8 +2,14 @@ package com.spring.mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@ComponentScan({"com.spring.mongo.*"})
+@EntityScan("com.spring.mongo.*")
 public class MongoApplication {
 
 	public static void main(String[] args) {
