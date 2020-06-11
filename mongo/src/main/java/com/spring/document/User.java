d0ccb0fa02deca4e76c3931650e95d4729240c06
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-11 17:02:32 +0530

    user

--- mongo/src/main/java/com/spring/document/User.java
+++ mongo/src/main/java/com/spring/document/User.java
@@ -6,6 +6,10 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -39,6 +43,23 @@ public class User {
 		this.list = list;
 	}
 	
+	
+	
+	 @Override
+	  public String toString() {
+	      ObjectMapper mapper = new ObjectMapper();
+	      
+	   String jsonString = "";
+	   
+	   try {
+	      mapper.enable(SerializationFeature.INDENT_OUTPUT);
+	      jsonString = mapper.writeValueAsString(this);
+	    } catch (JsonProcessingException e) {
+	      e.printStackTrace();
+	    }
+	    
+	      return jsonString;
+	    }
 	
 	
 }
