5ac0f76d7fba021a9273702751e96c9e4b7adaa5
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-11 13:53:30 +0530

    controller config

---
+++ mongo/src/main/java/com/spring/controller/Controller.java
@@ -0,0 +1,24 @@
+package com.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.document.User;
+import com.spring.repository.UserRepository;
+
+@RestController
+@RequestMapping("/rest/users")
+public class Controller {
+
+	@Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/all")
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+}
