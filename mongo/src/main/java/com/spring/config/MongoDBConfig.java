6ec3129ee48789e167a7c7417d555ba61dea16da
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-06-11 19:02:18 +0530

    SonarLint rules followed

--- mongo/src/main/java/com/spring/config/MongoDBConfig.java
+++ mongo/src/main/java/com/spring/config/MongoDBConfig.java
@@ -3,19 +3,25 @@ package com.spring.config;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import com.spring.controller.Controller;
 import com.spring.document.Nest;
 import com.spring.document.User;
 import com.spring.repository.UserRepository;
 
+import ch.qos.logback.classic.Logger;
+
 @EnableMongoRepositories(basePackageClasses = UserRepository.class)
 @Configuration
 public class MongoDBConfig {
-
+	
+	private static final Logger log=(Logger) LoggerFactory.getLogger(Controller.class);
+	
 	
 	@Bean
 	CommandLineRunner commandLineRunner(UserRepository userRepository) {
@@ -29,8 +35,8 @@ public class MongoDBConfig {
 	
 	User user1=new User(1,"Soumya","Dip",o);
 	User user2=new User(2,"Soumya","Dip Chowdhury");
-	System.out.println(user1);
-	System.out.println(user1);
+	log.info("{}", user1);
+	log.info("{}", user2);
 	return string->{
 		userRepository.save(user1);
 		userRepository.save(user2);
